28b26f3ebc4d8ab5f0ed50fa1e76c692cfe34ef7
HosmerimBasar <135459886+hosmerim@users.noreply.github.com>
2026-03-30 21:26:44 +0300

    Create ultraparkour.py

---
+++ apps/ultraparkour.py
@@ -0,0 +1,16 @@
+# ULTRAPARKOUR - Geliştirici: HosmerimBasar
+# ETAKOK Konsol Sistemi Özel Sürümü
+
+import time
+
+def game_start():
+    print("----------------------------")
+    print("      ULTRAPARKOUR          ")
+    print("    By HosmerimBasar        ")
+    print("----------------------------")
+    time.sleep(1)
+    print("Sistem Yukleniyor...")
+    # Buraya ileride MAX98357 ses kodlarını ekleyeceğiz
+    print("OYUN BASLADI!")
+
+game_start()
